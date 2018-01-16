d42bdc8433566a325d90a03cb80d8e6b06827d04
ios-user <azadehafrasiabi@outlook.com>
2018-01-16 13:03:18 +0330

    change on texts and spinner added

--- RefreshInfiniteListView.ios.js
+++ RefreshInfiniteListView.ios.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const React = require('react');
-const {
-    PropTypes,
+var React = require('react');
+var {
+    PropTypes
 } = React;
-const ReactNative = require('react-native');
-const {
+var ReactNative = require('react-native');
+var {
     Image,
     View,
     Text,
@@ -15,7 +15,7 @@ const {
     ActivityIndicator,
 } = ReactNative;
 
-/* list status change graph
+/*list status change graph
 *
 *STATUS_NONE->[STATUS_REFRESH_IDLE, STATUS_INFINITE_IDLE, STATUS_INFINITE_LOADED_ALL]
 *STATUS_REFRESH_IDLE->[STATUS_NONE, STATUS_WILL_REFRESH]
@@ -27,18 +27,18 @@ const {
 *STATUS_INFINITE_LOADED_ALL->[STATUS_NONE]
 *
 */
-const
-    STATUS_NONE = 0,
-    STATUS_REFRESH_IDLE = 1,
-    STATUS_WILL_REFRESH = 2,
-    STATUS_REFRESHING = 3,
-    STATUS_INFINITE_IDLE = 4,
-    STATUS_WILL_INFINITE = 5,
-    STATUS_INFINITING = 6,
-    STATUS_INFINITE_LOADED_ALL = 7;
+var
+STATUS_NONE = 0,
+STATUS_REFRESH_IDLE = 1,
+STATUS_WILL_REFRESH = 2,
+STATUS_REFRESHING = 3,
+STATUS_INFINITE_IDLE = 4,
+STATUS_WILL_INFINITE = 5,
+STATUS_INFINITING = 6,
+STATUS_INFINITE_LOADED_ALL = 7;
 
-const DEFAULT_PULL_DISTANCE = 60;
-const DEFAULT_HF_HEIGHT = 50;
+var DEFAULT_PULL_DISTANCE = 60;
+var DEFAULT_HF_HEIGHT = 50;
 
 module.exports = React.createClass({
     propTypes: {
@@ -59,129 +59,100 @@ module.exports = React.createClass({
             pullDistance: DEFAULT_PULL_DISTANCE,
             renderEmptyRow: () => {
                 return (
-                    <View style={{ height:Dimensions.get('window').height * 2 / 3, justifyContent:'center', alignItems:'center' }}>
-                        <Text style={{ fontSize:40, fontWeight:'800', color:'red' }}>
-                            have no data
-                        </Text>
+                    <View style={{height:Dimensions.get('window').height*2/3, justifyContent:'center',alignItems:'center'}}>
+                        <ActivityIndicator
+                        size='large'
+                        color='#20A399'
+                        animating={true}/>
                     </View>
-                );
+                )
             },
-            renderHeaderRefreshIdle: () => {
-                return (
-                    <View style={{ flex:1, height:DEFAULT_HF_HEIGHT, justifyContent:'center', alignItems:'center' }}>
-                        <Text style={styles.text}>
-                        pull down refresh...
-                    </Text>
-                        <Image
-                            source={require('./pull_arrow.png')}
-                            resizeMode={Image.resizeMode.stretch}
-                            style={styles.image}
-                        />
-                    </View>
-                );
-            },
-            renderHeaderWillRefresh: () => {
-                return (
-                    <View style={{ height:DEFAULT_HF_HEIGHT, justifyContent:'center', alignItems:'center' }}>
-                        <Text style={styles.text}>
-                        release to refresh...
-                    </Text>
-                        <Image
-                            source={require('./pull_arrow.png')}
-                            resizeMode={Image.resizeMode.stretch}
-                            style={[styles.image, styles.imageRotate]}
-                        />
-                    </View>
-                );
-            },
-            renderHeaderRefreshing: () => {
-                return (
-                    <View style={{ height:DEFAULT_HF_HEIGHT, justifyContent:'center', alignItems:'center' }}>
-                        <Text style={styles.text}>
+            renderHeaderRefreshIdle: () => {return (
+                <View style={{flex:1, height:DEFAULT_HF_HEIGHT, justifyContent:'center', alignItems:'center'}}>
+                     <ActivityIndicator
+                     size='small'
+                     color='#20A399'
+                     animating={true}/>
+                </View>
+            )},
+            renderHeaderWillRefresh: () => {return (
+                <View style={{height:DEFAULT_HF_HEIGHT, justifyContent:'center', alignItems:'center'}}>
+                    <ActivityIndicator
+                    size='small'
+                    color='#20A399'
+                    animating={true}/>
+                </View>
+            )},
+            renderHeaderRefreshing: () => {return (
+                <View style={{height:DEFAULT_HF_HEIGHT, justifyContent:'center', alignItems:'center'}}>
+                    <Text style={styles.text}>
                         refreshing...
                     </Text>
 
-                        <ActivityIndicator
-                            size='small'
-                            animating />
-                    </View>
-                );
-            },
-            renderFooterInifiteIdle: () => {
-                return (
-                    <View style={{ height:DEFAULT_HF_HEIGHT, justifyContent:'center', alignItems:'center' }}>
-                        <Image
-                            source={require('./pull_arrow.png')}
-                            resizeMode={Image.resizeMode.stretch}
-                            style={[styles.image, styles.imageRotate]}
-                        />
-                        <Text style={styles.text}>
-                        pull up to load more...
-                    </Text>
-                    </View>
-                );
-            },
-            renderFooterWillInifite: () => {
-                return (
-                    <View style={{ height:DEFAULT_HF_HEIGHT, justifyContent:'center', alignItems:'center' }}>
-                        <Image
-                            source={require('./pull_arrow.png')}
-                            resizeMode={Image.resizeMode.stretch}
-                            style={styles.image}
-                        />
-                        <Text style={styles.text}>
-                        release to load more...
-                    </Text>
-                    </View>
-                );
-            },
-            renderFooterInifiting: () => {
-                return (
-                    <View style={{ height:DEFAULT_HF_HEIGHT, justifyContent:'center', alignItems:'center' }}>
-                        <ActivityIndicator
-                            size='small'
-                            animating />
-                        <Text style={styles.text}>
+                    <ActivityIndicator
+                        size='small'
+                        animating={true}/>
+                </View>
+            )},
+            renderFooterInifiteIdle: () => {return (
+                <View style={{height:DEFAULT_HF_HEIGHT, justifyContent:'center', alignItems:'center'}}>
+                    <ActivityIndicator
+                     size='small'
+                     color='#20A399'
+                     animating={true}/>
+                </View>
+            )},
+            renderFooterWillInifite: () => {return (
+                <View style={{height:DEFAULT_HF_HEIGHT, justifyContent:'center', alignItems:'center'}}>
+                    <ActivityIndicator
+                     size='small'
+                     color='#20A399'
+                     animating={true}/>
+                </View>
+            )},
+            renderFooterInifiting: () => {return (
+                <View style={{height:DEFAULT_HF_HEIGHT, justifyContent:'center', alignItems:'center'}}>
+                    <ActivityIndicator
+                        size='small'
+                        animating={true}/>
+                    <Text style={styles.text}>
                         loading...
                     </Text>
-                    </View>
-                );
-            },
-            renderFooterInifiteLoadedAll: () => {
-                return (
-                    <View style={{ height:DEFAULT_HF_HEIGHT, justifyContent:'center', alignItems:'center' }}>
-                        <Text style={styles.text}>
-                        have loaded all data
+                </View>
+            )},
+            renderFooterInifiteLoadedAll: () => { return (
+                <View style={{height:DEFAULT_HF_HEIGHT, justifyContent:'center', alignItems:'center'}}>
+                    <Text style={styles.text}>
+                        داده ای برای نمایش وجود ندارد.
                     </Text>
-                    </View>
-                );
-            },
+                </View>
+            )},
             loadedAllData: () => {
                 return false;
             },
             onRefresh: () => {
-                console.log('onRefresh');
+                console.log("onRefresh");
             },
             onInfinite: () => {
-                console.log('onInfinite');
+                console.log("onInfinite");
             },
         };
     },
-    getInitialState () {
+    getInitialState() {
         return {
             status: STATUS_NONE,
             isLoadedAllData: false,
-        };
+        }
     },
-    renderRow (text) {
+    renderRow(text) {
         if (this.dataSource) {
             return this.props.renderEmptyRow(text);
         } else {
             return this.props.renderRow(text);
         }
     },
-    renderHeader () {
-        const status = this.state.status;
+    renderHeader() {
+        var status = this.state.status;
         if (status === STATUS_REFRESH_IDLE) {
             return this.props.renderHeaderRefreshIdle();
         }
@@ -193,8 +164,8 @@ module.exports = React.createClass({
         }
         return null;
     },
-    renderFooter () {
-        const status = this.state.status;
+    renderFooter() {
+        var status = this.state.status;
         this.footerIsRender = true;
         if (status === STATUS_INFINITE_IDLE) {
             return this.props.renderFooterInifiteIdle();
@@ -211,85 +182,86 @@ module.exports = React.createClass({
         this.footerIsRender = false;
         return null;
     },
-    handleResponderGrant (event) {
-        const nativeEvent = event.nativeEvent;
-        if (!nativeEvent.contentInset || this.state.status !== STATUS_NONE) {
+    handleResponderGrant(event) {
+        var nativeEvent = event.nativeEvent;
+        if (!nativeEvent.contentInset || this.state.status!==STATUS_NONE) {
             return;
         }
-        let y0 = nativeEvent.contentInset.top + nativeEvent.contentOffset.y;
+        var y0 = nativeEvent.contentInset.top + nativeEvent.contentOffset.y;
         if (y0 < 0) {
-            this.setState({ status:STATUS_REFRESH_IDLE });
+            this.setState({status:STATUS_REFRESH_IDLE});
             return;
         }
         y0 = nativeEvent.contentInset.top + nativeEvent.contentOffset.y +
-        nativeEvent.layoutMeasurement.height - nativeEvent.contentSize.height;
-        if (y0 > 0) {
+        nativeEvent.layoutMeasurement.height-nativeEvent.contentSize.height;
+        if (y0 > 0 ) {
             if (!this.props.loadedAllData()) {
-                this.initialInfiniteOffset = (y0 > 0 ? y0 : 0);
-                this.setState({ status:STATUS_INFINITE_IDLE });
+                this.initialInfiniteOffset = (y0>0?y0:0);
+                this.setState({status:STATUS_INFINITE_IDLE});
             } else {
-                this.setState({ status:STATUS_INFINITE_LOADED_ALL });
+                this.setState({status:STATUS_INFINITE_LOADED_ALL});
             }
         }
     },
-    hideHeader () {
-        this.setState({ status:STATUS_NONE });
+    hideHeader() {
+        this.setState({status:STATUS_NONE});
     },
-    hideFooter () {
-        this.setState({ status:STATUS_NONE });
+    hideFooter() {
+        this.setState({status:STATUS_NONE});
     },
-    handleResponderRelease (event) {
-        const status = this.state.status;
+    handleResponderRelease(event) {
+        var status = this.state.status;
         if (status === STATUS_REFRESH_IDLE) {
-            this.setState({ status:STATUS_NONE });
+            this.setState({status:STATUS_NONE});
         } else if (status === STATUS_WILL_REFRESH) {
-            this.setState({ status:STATUS_REFRESHING });
+            this.setState({status:STATUS_REFRESHING});
             this.props.onRefresh();
         } else if (status === STATUS_INFINITE_IDLE) {
-            this.setState({ status:STATUS_NONE });
+            this.setState({status:STATUS_NONE});
         } else if (status === STATUS_WILL_INFINITE) {
-            this.setState({ status:STATUS_INFINITING });
+            this.setState({status:STATUS_INFINITING});
             this.props.onInfinite();
         } else if (status === STATUS_INFINITE_LOADED_ALL) {
-            this.setState({ status:STATUS_NONE });
+            this.setState({status:STATUS_NONE});
         }
     },
-    handleScroll (event) {
-        const nativeEvent = event.nativeEvent;
-        const status = this.state.status;
-        if (status === STATUS_REFRESH_IDLE || status === STATUS_WILL_REFRESH) {
-            const y = nativeEvent.contentInset.top + nativeEvent.contentOffset.y;
-            if (status !== STATUS_WILL_REFRESH && y < -this.props.pullDistance) {
-                this.setState({ status:STATUS_WILL_REFRESH });
-            } else if (status === STATUS_WILL_REFRESH && y >= -this.props.pullDistance) {
-                this.setState({ status:STATUS_REFRESH_IDLE });
+    handleScroll(event) {
+        var nativeEvent = event.nativeEvent;
+        var status = this.state.status;
+        if (status===STATUS_REFRESH_IDLE || status===STATUS_WILL_REFRESH) {
+            var y = nativeEvent.contentInset.top + nativeEvent.contentOffset.y
+            if (status!==STATUS_WILL_REFRESH && y<-this.props.pullDistance) {
+                this.setState({status:STATUS_WILL_REFRESH});
+            } else if (status===STATUS_WILL_REFRESH && y>=-this.props.pullDistance) {
+                this.setState({status:STATUS_REFRESH_IDLE});
             }
             return;
         }
 
-        if (status === STATUS_INFINITE_IDLE || status === STATUS_WILL_INFINITE) {
-            let y = nativeEvent.contentInset.top + nativeEvent.contentOffset.y + nativeEvent.layoutMeasurement.height
-            - nativeEvent.contentSize.height - this.initialInfiniteOffset;
+        if (status===STATUS_INFINITE_IDLE || status===STATUS_WILL_INFINITE) {
+            var y = nativeEvent.contentInset.top + nativeEvent.contentOffset.y + nativeEvent.layoutMeasurement.height
+            -nativeEvent.contentSize.height-this.initialInfiniteOffset;
             if (this.footerIsRender) {
                 y += this.props.footerHeight;
             }
-            if (status !== STATUS_WILL_INFINITE && y > this.props.pullDistance) {
-                this.setState({ status:STATUS_WILL_INFINITE });
-            } else if (status === STATUS_WILL_INFINITE && y <= this.props.pullDistance) {
-                this.setState({ status:STATUS_INFINITE_IDLE });
+            if (status!==STATUS_WILL_INFINITE && y>this.props.pullDistance) {
+                this.setState({status:STATUS_WILL_INFINITE});
+            } else if (status===STATUS_WILL_INFINITE && y<=this.props.pullDistance) {
+                this.setState({status:STATUS_INFINITE_IDLE});
             }
         }
     },
-    render () {
+    render() {
         this.dataSource = null;
         if (!this.props.dataSource.getRowCount()) {
-            const DataSource = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-            this.dataSource = DataSource.cloneWithRows(['']);
+            var DataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+            this.dataSource = DataSource.cloneWithRows([""]);
+
         }
         return (
             <ListView
                 {...this.props}
-                dataSource={this.dataSource ? this.dataSource : this.props.dataSource}
+                dataSource={this.dataSource?this.dataSource:this.props.dataSource}
                 renderRow={this.renderRow}
                 renderHeader={this.renderHeader}
                 renderFooter={this.renderFooter}
@@ -297,11 +269,11 @@ module.exports = React.createClass({
                 onResponderRelease={this.handleResponderRelease}
                 onScroll={this.handleScroll}
                 />
-        );
-    },
+        )
+    }
 });
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
     text: {
         fontSize:16,
     },
@@ -310,6 +282,6 @@ const styles = StyleSheet.create({
         height:32,
     },
     imageRotate: {
-        transform:[{ rotateX: '180deg' }],
-    },
+        transform:[{rotateX: '180deg'},],
+    }
 });
